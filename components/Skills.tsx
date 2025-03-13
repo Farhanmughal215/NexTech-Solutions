f4917429cfec2f23c82b9b872038a3334bef13b2
farhanzafar215 <44663633+Farhanmughal215@users.noreply.github.com>
2025-03-13 04:02:20 -0700

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,49 +7,49 @@ const Skills = () => {
     <div className="wrapper">
       <Title text="Skills" icon={<GiSkills />} />
       <div className="flex gap-4 flex-wrap">
-       <SkillsInput title="Web Development" />
-<SkillsInput title="WordPress Design" />
-<SkillsInput title="WP Theme Customization" />
-<SkillsInput title="WordPress Security" />
-<SkillsInput title="Landing Pages" />
-<SkillsInput title="HTML5" />
-<SkillsInput title="CSS3" />
-<SkillsInput title="Bootstrap" />
-<SkillsInput title="Tailwind CSS" />
-<SkillsInput title="JavaScript" />
-<SkillsInput title="JQuery" />
-<SkillsInput title="React Js" />
-<SkillsInput title="PHP & MySQL" />
-<SkillsInput title="No-Code Development" />
-<SkillsInput title="AI Integration" />
-<SkillsInput title="ChatBots" />
-<SkillsInput title="AI Automation Tools" />
-<SkillsInput title="AI Workflow Automation" />
-<SkillsInput title="OpenAI API Integration" />
-<SkillsInput title="AI Video & Image" />
-<SkillsInput title="SEO Optimization" />
-<SkillsInput title="Digital Marketing" />
-<SkillsInput title="Social Media Marketing" />
-<SkillsInput title="Email Marketing" />
-<SkillsInput title="Google Analytics" />
-<SkillsInput title="SEO Content Optimization" />
-<SkillsInput title="UI/UX Design" />
-<SkillsInput title="Canva Expert" />
-<SkillsInput title="WhitePaper Designs" />
-<SkillsInput title="Proposals Writing" />
-<SkillsInput title="Project Management" />
-<SkillsInput title="Team Leadership" />
-<SkillsInput title="Git & GitHub" />
-<SkillsInput title="Website Performance Optimization" />
-<SkillsInput title="Web Hosting & Deployment" />
-<SkillsInput title="Memecoin Website Design" />
-<SkillsInput title="Tokenomics & Community Building" />
-<SkillsInput title="How to Launch a Memecoin" />
-<SkillsInput title="Launching a Memecoin on Pump.Fun" />
-<SkillsInput title="Crypto Project Management" />
-<SkillsInput title="Crypto Marketing & Growth" />
-<SkillsInput title="Meme Coin Shilling" />
-<SkillsInput title="Telegram & Twitter Marketing" />
+    <SkillsInput title="Web Development" link="#" />
+<SkillsInput title="WordPress Design" link="#" />
+<SkillsInput title="WP Theme Customization" link="#" />
+<SkillsInput title="WordPress Security" link="#" />
+<SkillsInput title="Landing Pages" link="#" />
+<SkillsInput title="HTML5" link="#" />
+<SkillsInput title="CSS3" link="#" />
+<SkillsInput title="Bootstrap" link="#" />
+<SkillsInput title="Tailwind CSS" link="#" />
+<SkillsInput title="JavaScript" link="#" />
+<SkillsInput title="JQuery" link="#" />
+<SkillsInput title="React Js" link="#" />
+<SkillsInput title="PHP & MySQL" link="#" />
+<SkillsInput title="No-Code Development" link="#" />
+<SkillsInput title="AI Integration" link="#" />
+<SkillsInput title="ChatBots" link="#" />
+<SkillsInput title="AI Automation Tools" link="#" />
+<SkillsInput title="AI Workflow Automation" link="#" />
+<SkillsInput title="OpenAI API Integration" link="#" />
+<SkillsInput title="AI Video & Image" link="#" />
+<SkillsInput title="SEO Optimization" link="#" />
+<SkillsInput title="Digital Marketing" link="#" />
+<SkillsInput title="Social Media Marketing" link="#" />
+<SkillsInput title="Email Marketing" link="#" />
+<SkillsInput title="Google Analytics" link="#" />
+<SkillsInput title="SEO Content Optimization" link="#" />
+<SkillsInput title="UI/UX Design" link="#" />
+<SkillsInput title="Canva Expert" link="#" />
+<SkillsInput title="WhitePaper Designs" link="#" />
+<SkillsInput title="Proposals Writing" link="#" />
+<SkillsInput title="Project Management" link="#" />
+<SkillsInput title="Team Leadership" link="#" />
+<SkillsInput title="Git & GitHub" link="#" />
+<SkillsInput title="Website Performance Optimization" link="#" />
+<SkillsInput title="Web Hosting & Deployment" link="#" />
+<SkillsInput title="Memecoin Website Design" link="#" />
+<SkillsInput title="Tokenomics & Community Building" link="#" />
+<SkillsInput title="How to Launch a Memecoin" link="#" />
+<SkillsInput title="Launching a Memecoin on Pump.Fun" link="#" />
+<SkillsInput title="Crypto Project Management" link="#" />
+<SkillsInput title="Crypto Marketing & Growth" link="#" />
+<SkillsInput title="Meme Coin Shilling" link="#" />
+<SkillsInput title="Telegram & Twitter Marketing" link="#" />
 <SkillsInput title="More" link="https://www.linkedin.com/in/farhanzafartech/" />
         
       </div>
