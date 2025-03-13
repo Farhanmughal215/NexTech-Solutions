1ad0d6e786e7eb1cb7bc0040f1c37417a8a46c38
farhanzafar215 <44663633+Farhanmughal215@users.noreply.github.com>
2025-03-13 04:35:57 -0700

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
       <div className="w-44 h-44 rounded-full bg-black">
         <Image
           className="w-full h-full rounded-full border-[1px] border-indigo-700 p-2 object-cover"
-          src={ProfileImg}
+          src={ifhprofileImg}
           alt="ProfileImg"
         />
       </div>
